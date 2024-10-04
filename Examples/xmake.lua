1a6ad0cb1f3e920eaf7bf88ce689bb8eb192994c
Cinition <cornee.n@gmail.com>
2024-10-04 17:02:58 +0200

    Implement SDL window + imgui

--- Examples/xmake.lua
+++ Examples/xmake.lua
@@ -1,10 +1,11 @@
 set_languages("cxx17")
-add_rules("mode.debug", "mode.release")
 
-add_requires("imgui")
+add_requires("imgui v1.91.1-docking", {configs = {opengl3 = true, sdl2 = true}})
+add_requires("glad")
+add_requires("libsdl")
 
 target("ImGimzo")
-    set_kind("binary")
-    add_files("main.cpp")
+    set_kind("console")
+    add_files("*.cpp")
     add_includedirs( "../" )
-    add_packages("imgui")
+    add_packages("imgui", "libsdl", "glad")
