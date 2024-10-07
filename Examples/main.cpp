df130c225293c04c66c4ef9b063c41980124289c
Cinition <cornee.n@gmail.com>
2024-10-07 16:32:44 +0200

    Working rotating opengl cube

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -23,22 +23,22 @@
 #include <ImGizmo.h>
 
 struct Cube {
-    glm::vec3 vertices[8] {
-        {-0.5, -0.5, -0.5f}, // 0
-        {0.5, -0.5, -0.5f},  // 1
-        {0.5, -0.5, 0.5f},   // 2
-        {-0.5, -0.5, 0.5f},  // 3
-        {-0.5, 0.5, -0.5f},  // 4
-        {0.5, 0.5, -0.5f},   // 5
-        {0.5, 0.5, 0.5f},    // 6
-        {-0.5, 0.5, 0.5f},   // 7
+    float vertices[24] {
+        -0.5, -0.5, -0.5f, // 0
+        0.5, -0.5, -0.5f,  // 1
+        0.5, -0.5, 0.5f,   // 2
+        -0.5, -0.5, 0.5f,  // 3
+        -0.5, 0.5, -0.5f,  // 4
+        0.5, 0.5, -0.5f,   // 5
+        0.5, 0.5, 0.5f,    // 6
+        -0.5, 0.5, 0.5f,   // 7
     };
 
-    float indices[36] {
+    int indices[36] {
         0, 1, 4, 4, 1, 5, // front
         1, 2, 5, 5, 2, 6, // left
         2, 3, 6, 6, 3, 7, // back
-        3, 4, 7, 7, 4, 8, // right
+        3, 0, 7, 7, 0, 4, // right
         4, 5, 7, 7, 5, 6, // top
         0, 1, 3, 3, 1, 2, // bottom
     };
@@ -53,26 +53,23 @@ struct Shader {
 
     Shader() {
         const char* vShaderCode =
-            "#version 330 core\n"
+            "#version 460 core\n"
             "layout (location = 0) in vec3 pos;\n"
-            "out vec3 normal;\n"
             "uniform mat4 modelMatrix;\n"
             "uniform mat4 view;\n"
             "uniform mat4 projection;\n"
             "void main()\n"
             "{\n"
-            "    gl_Position = projection * view * modelMatrix * vec4(pos, 1.0);\n"
-            "    normal = normalize(gl_NormalMatrix * gl_Normal.xyz);\n"
-            "}\n";
+            "    gl_Position = projection * view * modelMatrix * vec4(pos.x, pos.y, pos.z, 1.0f);\n"
+            "}\0";
 
         const char * fShaderCode =
-            "#version 330 core\n"
-            "in vec3 normal;\n"
+            "#version 460 core\n"
             "out vec4 FragColor;\n"
             "void main()\n"
             "{\n"
-            "    FragColor = vec4(normal, 1.0);\n"
-            "}\n";
+            "    FragColor = vec4(0.8f, 0.12f, 0.12f, 1.0f);\n"
+            "}\0";
 
         unsigned int vertex, fragment;
 
@@ -126,7 +123,6 @@ int main(int argc, char** argv) {
     }
 
     glEnable(GL_DEPTH_TEST);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     Cube cube1;
 
@@ -152,7 +148,6 @@ int main(int argc, char** argv) {
 
     // Init Shader
     Shader shader = Shader();
-    std::printf(std::to_string(shader.ID).c_str());
 
     // Init ImGui
     IMGUI_CHECKVERSION();
@@ -171,14 +166,41 @@ int main(int argc, char** argv) {
         int displayW, displayH;
         glfwGetFramebufferSize(window, &displayW, &displayH);
 
-        glUseProgram(shader.ID);
+        glViewport(0, 0, displayW, displayH);
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Render ImGui
+        {
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            ImGui::ShowDemoWindow();
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
 
         // Render Scene
         {
+            glUseProgram(shader.ID);
+
+            static float rotation = 0.f;
+            rotation += 0.01f;
+            glm::mat4 rot = glm::mat4(1.f);
             glm::mat4 view = glm::mat4(1.f);
             glm::mat4 proj = glm::mat4(1.f);
-            proj = glm::perspective(glm::radians(45.f), (float)displayW / (float)displayH, 0.1f, 100.f);
+            rot = glm::rotate(rot, rotation, glm::vec3(0.f, 1.f, 0.f));
             view = glm::translate(view, glm::vec3(0.f, 0.f, -10.f));
+            view = view * rot;
+            proj = glm::perspective(glm::radians(45.f), 1.778f, 0.1f, 100.f);
+
+            int viewMatrixLocation = glGetUniformLocation(shader.ID, "view");
+            glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));
+
+            int projectionMatrixLocation = glGetUniformLocation(shader.ID, "projection");
+            glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(proj));
 
             glBindVertexArray(VAO);
             glm::mat4 modelMatrix = glm::mat4(1.f);
@@ -190,21 +212,6 @@ int main(int argc, char** argv) {
         }
         glBindVertexArray(0);
 
-        // Render ImGui
-        {
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            ImGui::ShowDemoWindow();
-
-            ImGui::Render();
-        }
-
-        glViewport(0, 0, displayW, displayH);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
 
