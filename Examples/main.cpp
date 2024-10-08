364e4242e281b02641064ff3089889bc5a769ddd
Cinition <cornee.n@gmail.com>
2024-10-08 15:24:01 +0200

    Colored cube

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -1,13 +1,11 @@
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/vector_float2.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/trigonometric.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#include <array>
-#include <fstream>
-#include <sstream>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -23,24 +21,48 @@
 #include <ImGizmo.h>
 
 struct Cube {
-    float vertices[24] {
-        -0.5, -0.5, -0.5f, // 0
-        0.5, -0.5, -0.5f,  // 1
-        0.5, -0.5, 0.5f,   // 2
-        -0.5, -0.5, 0.5f,  // 3
-        -0.5, 0.5, -0.5f,  // 4
-        0.5, 0.5, -0.5f,   // 5
-        0.5, 0.5, 0.5f,    // 6
-        -0.5, 0.5, 0.5f,   // 7
-    };
+    float vertices[216] = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 
-    int indices[36] {
-        0, 1, 4, 4, 1, 5, // front
-        1, 2, 5, 5, 2, 6, // left
-        2, 3, 6, 6, 3, 7, // back
-        3, 0, 7, 7, 0, 4, // right
-        4, 5, 7, 7, 5, 6, // top
-        0, 1, 3, 3, 1, 2, // bottom
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
 
     glm::vec3 position {0.f, 0.f, 0.f};
@@ -54,21 +76,25 @@ struct Shader {
     Shader() {
         const char* vShaderCode =
             "#version 460 core\n"
-            "layout (location = 0) in vec3 pos;\n"
+            "layout (location = 0) in vec3 aPosition;\n"
+            "layout (location = 1) in vec3 aNormal;\n"
             "uniform mat4 modelMatrix;\n"
             "uniform mat4 view;\n"
             "uniform mat4 projection;\n"
+            "out vec3 Color;\n"
             "void main()\n"
             "{\n"
-            "    gl_Position = projection * view * modelMatrix * vec4(pos.x, pos.y, pos.z, 1.0f);\n"
+            "    gl_Position = projection * view * modelMatrix * vec4(aPosition.x, aPosition.y, aPosition.z, 1.0f);\n"
+            "    Color = aNormal;\n"
             "}\0";
 
         const char * fShaderCode =
             "#version 460 core\n"
+            "in vec3 Color;\n"
             "out vec4 FragColor;\n"
             "void main()\n"
             "{\n"
-            "    FragColor = vec4(0.8f, 0.12f, 0.12f, 1.0f);\n"
+            "    FragColor = vec4(abs(Color), 1.0f);\n"
             "}\0";
 
         unsigned int vertex, fragment;
@@ -98,7 +124,7 @@ void errorCallBack(int error, const char* desc) {
 int main(int argc, char** argv) {
     if(!glfwInit())
     {
-        // Initialization failed
+        std::printf("GLFW Initialization failed");
         exit(EXIT_FAILURE);
     }
 
@@ -126,21 +152,20 @@ int main(int argc, char** argv) {
 
     Cube cube1;
 
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube1.vertices), cube1.vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube1.indices), cube1.indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -176,7 +201,9 @@ int main(int argc, char** argv) {
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            ImGui::ShowDemoWindow();
+            ImGui::Begin("ImGizmo Demo");
+
+            ImGui::End();
 
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -184,14 +211,33 @@ int main(int argc, char** argv) {
 
         // Render Scene
         {
+            // Mouse camera rotation
+            static glm::vec2 rotation     = glm::vec2(0.f);
+            static glm::vec2 lastMousePos = glm::vec2(0.f);
+
+            double xPos, yPos;
+            glfwGetCursorPos(window, &xPos, &yPos);
+            glm::vec2 mousePos = glm::vec2(xPos, yPos);
+            {
+                if( glfwGetMouseButton(window, 0) == GLFW_PRESS) {
+                    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+                    glm::vec2 relativePos = lastMousePos - mousePos;
+                    rotation += (relativePos * 0.005f) * glm::vec2(-1.f, -1.f);
+                }
+                else {
+                    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+                }
+            }
+            lastMousePos.x = xPos;
+            lastMousePos.y = yPos;
+
             glUseProgram(shader.ID);
 
-            static float rotation = 0.f;
-            rotation += 0.01f;
             glm::mat4 rot = glm::mat4(1.f);
             glm::mat4 view = glm::mat4(1.f);
             glm::mat4 proj = glm::mat4(1.f);
-            rot = glm::rotate(rot, rotation, glm::vec3(0.f, 1.f, 0.f));
+            rot = glm::rotate(rot, rotation.y, glm::vec3(1.f, 0.f, 0.f));
+            rot = glm::rotate(rot, rotation.x, glm::vec3(0.f, 1.f, 0.f));
             view = glm::translate(view, glm::vec3(0.f, 0.f, -10.f));
             view = view * rot;
             proj = glm::perspective(glm::radians(45.f), 1.778f, 0.1f, 100.f);
@@ -202,15 +248,13 @@ int main(int argc, char** argv) {
             int projectionMatrixLocation = glGetUniformLocation(shader.ID, "projection");
             glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(proj));
 
-            glBindVertexArray(VAO);
-
             glm::mat4 modelMatrix = glm::mat4(1.f);
             modelMatrix = glm::translate(modelMatrix, cube1.position);
             int modelMatrixLocation = glGetUniformLocation(shader.ID, "modelMatrix");
-
             glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(modelMatrix));
-            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
             glBindVertexArray(0);
         }
 
