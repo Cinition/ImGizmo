9a096a925c23bd2cade3499eea7ecbd954551979
Cornee Nieuwenhuis <37161255+Cinition@users.noreply.github.com>
2025-05-20 17:16:43 +0200

    Line drawing + working on line mouse detection

--- ImGizmo.cpp
+++ ImGizmo.cpp
@@ -4,8 +4,6 @@
 #include <cstdio>
 
 #include "ImGizmo.h"
-#include "ImGizmo_Math.h"
-#include "ImGizmo_Internal.h"
 
 #include "imgui.h"
 #include "imgui_internal.h"
@@ -14,7 +12,101 @@
 ImGizmoContext* GImGizmo = nullptr;
 #endif
 
+struct ImGizmoSpace {
+    ImGuiID ID;
+    ImGizmoMatrix viewMatrix;
+    ImGizmoMatrix projMatrix;
+    ImDrawList* drawList;
+    ImRect frameRect;
+
+    bool initialized;
+};
+
+struct ImGizmoContext {
+    ImGizmoSpace currentSpace;
+    // TODO: add global styling options, like in imgui
+};
+
 void InitializeContext(ImGizmoContext* context) {};
+
+void FPU_MatrixF_x_MatrixF(const float* a, const float* b, float* r)
+{
+  r[0] = a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12];
+  r[1] = a[0] * b[1] + a[1] * b[5] + a[2] * b[9] + a[3] * b[13];
+  r[2] = a[0] * b[2] + a[1] * b[6] + a[2] * b[10] + a[3] * b[14];
+  r[3] = a[0] * b[3] + a[1] * b[7] + a[2] * b[11] + a[3] * b[15];
+
+  r[4] = a[4] * b[0] + a[5] * b[4] + a[6] * b[8] + a[7] * b[12];
+  r[5] = a[4] * b[1] + a[5] * b[5] + a[6] * b[9] + a[7] * b[13];
+  r[6] = a[4] * b[2] + a[5] * b[6] + a[6] * b[10] + a[7] * b[14];
+  r[7] = a[4] * b[3] + a[5] * b[7] + a[6] * b[11] + a[7] * b[15];
+
+  r[8] = a[8] * b[0] + a[9] * b[4] + a[10] * b[8] + a[11] * b[12];
+  r[9] = a[8] * b[1] + a[9] * b[5] + a[10] * b[9] + a[11] * b[13];
+  r[10] = a[8] * b[2] + a[9] * b[6] + a[10] * b[10] + a[11] * b[14];
+  r[11] = a[8] * b[3] + a[9] * b[7] + a[10] * b[11] + a[11] * b[15];
+
+  r[12] = a[12] * b[0] + a[13] * b[4] + a[14] * b[8] + a[15] * b[12];
+  r[13] = a[12] * b[1] + a[13] * b[5] + a[14] * b[9] + a[15] * b[13];
+  r[14] = a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14];
+  r[15] = a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15];
+}
+
+ImGizmoVec3 ImGizmoMatrix::Pos() const {
+    ImGizmoVec3 vec;
+
+    vec.x = this->m4x4[3][0];
+    vec.y = this->m4x4[3][1];
+    vec.z = this->m4x4[3][2];
+
+    return vec;
+}
+
+ImGizmoMatrix ImGizmoMatrix::operator* (const ImGizmoMatrix& _matrix) const {
+    ImGizmoMatrix out;
+    FPU_MatrixF_x_MatrixF(this->m16, _matrix.m16, out.m16);
+    return out;
+}
+
+ImGizmoVec3 ImGizmoVec3::Transform(const ImGizmoMatrix& _matrix) {
+    ImGizmoVec4 in;
+    ImGizmoVec3 out;
+
+    in.x = this->x * _matrix.m4x4[0][0] + this->y * _matrix.m4x4[1][0] + this->z * _matrix.m4x4[2][0] + _matrix.m4x4[3][0];
+    in.y = this->x * _matrix.m4x4[0][1] + this->y * _matrix.m4x4[1][1] + this->z * _matrix.m4x4[2][1] + _matrix.m4x4[3][1];
+    in.z = this->x * _matrix.m4x4[0][2] + this->y * _matrix.m4x4[1][2] + this->z * _matrix.m4x4[2][2] + _matrix.m4x4[3][2];
+    in.w = this->x * _matrix.m4x4[0][3] + this->y * _matrix.m4x4[1][3] + this->z * _matrix.m4x4[2][3] + _matrix.m4x4[3][3];
+
+    in.x /= in.w;
+    in.y /= in.w;
+    in.z /= in.w;
+
+    this->x = in.x;
+    this->y = in.y;
+    this->z = in.z;
+
+    return *this;
+}
+
+ImGizmoVec4 ImGizmoVec4::Transform(const ImGizmoMatrix& _matrix) {
+    ImGizmoVec4 out;
+
+    out.x = this->x * _matrix.m4x4[0][0] + this->y * _matrix.m4x4[1][0] + this->z * _matrix.m4x4[2][0] + _matrix.m4x4[3][0];
+    out.y = this->x * _matrix.m4x4[0][1] + this->y * _matrix.m4x4[1][1] + this->z * _matrix.m4x4[2][1] + _matrix.m4x4[3][1];
+    out.z = this->x * _matrix.m4x4[0][2] + this->y * _matrix.m4x4[1][2] + this->z * _matrix.m4x4[2][2] + _matrix.m4x4[3][2];
+    out.w = this->x * _matrix.m4x4[0][3] + this->y * _matrix.m4x4[1][3] + this->z * _matrix.m4x4[2][3] + _matrix.m4x4[3][3];
+
+    out.x /= out.w;
+    out.y /= out.w;
+    out.z /= out.w;
+
+    this->x = out.x;
+    this->y = out.y;
+    this->z = out.z;
+    this->w = out.w;
+
+    return *this;
+}
 
 namespace IMGIZMO_NAMESPACE {
 
@@ -101,7 +193,7 @@ namespace IMGIZMO_NAMESPACE {
     // DRAW CALLS
     // ----------------------------------------------------------------------------------------------------------------------------------
 
-    bool DrawPoint(const char* _id, float* _x, float* _y, float* _z) {
+    bool DrawPoint(const char* _id, ImGizmoVec3 point) {
         IM_ASSERT_USER_ERROR(GImGizmo != nullptr, "Current context is empty. Did you call ImGizmo::CreateContext()?");
         IM_ASSERT_USER_ERROR(GImGizmo->currentSpace.initialized == true, "Current ImGizmo space is empty. Did you call ImGizmo::Begin()?");
 
@@ -115,7 +207,6 @@ namespace IMGIZMO_NAMESPACE {
         ImDrawList& drawList = *GImGizmo->currentSpace.drawList;
 
         // Draw debug point
-        ImGizmoVec point = ImGizmoVec(*_x, *_y, *_z, 1.f);
         point.Transform(viewMatrix);
         point.Transform(projMatrix);
 
@@ -137,6 +228,57 @@ namespace IMGIZMO_NAMESPACE {
         }
         else {
             drawList.AddCircle({point.x, point.y}, 1.f, ImGui::GetColorU32({1.f, 1.f, 1.f, 1.f}));
+        }
+
+        return hovering;
+    }
+
+    bool DrawLine(const char* _id, ImGizmoVec3 point1, ImGizmoVec3 point2) {
+        IM_ASSERT_USER_ERROR(GImGizmo != nullptr, "Current context is empty. Did you call ImGizmo::CreateContext()?");
+        IM_ASSERT_USER_ERROR(GImGizmo->currentSpace.initialized == true, "Current ImGizmo space is empty. Did you call ImGizmo::Begin()?");
+
+        const ImGizmoMatrix& viewMatrix = GImGizmo->currentSpace.viewMatrix;
+        const ImGizmoMatrix& projMatrix = GImGizmo->currentSpace.projMatrix;
+
+        ImDrawList& drawList = *GImGizmo->currentSpace.drawList;
+
+        // Draw debug point
+        point1.Transform(viewMatrix);
+        point1.Transform(projMatrix);
+        point2.Transform(viewMatrix);
+        point2.Transform(projMatrix);
+
+        const auto& max = GImGizmo->currentSpace.frameRect.Max;
+
+        ImVec2 screenPoint1 = {};
+        ImVec2 screenPoint2 = {};
+        screenPoint1.x = ((point1.x + 1.f) / 2.f ) * max.x;
+        screenPoint1.y = (1 - ((point1.y + 1.f) / 2.f )) * max.y;
+        screenPoint2.x = ((point2.x + 1.f) / 2.f ) * max.x;
+        screenPoint2.y = (1 - ((point2.y + 1.f) / 2.f )) * max.y;
+
+        bool hovering = false;
+        auto mousePos = ImGui::GetIO().MousePos;
+        auto point1ToMouse = mousePos - screenPoint1;
+        auto point1ToPoint2 = screenPoint2 - screenPoint1;
+        auto crossMagnitude = (point1ToPoint2.y * point1ToMouse.x) - (point1ToPoint2.x * point1ToMouse.y);
+
+        float normalizeValue = sqrt(pow(point1ToPoint2.x ,2.f) + pow(point1ToPoint2.y ,2.f));
+        auto normalizedP1ToP2 = ImVec2(point1ToPoint2.x / normalizeValue, point1ToPoint2.y / normalizeValue);
+        auto projectedPos = screenPoint1 + (normalizedP1ToP2 * crossMagnitude);
+
+        auto distance = pow(mousePos.x - projectedPos.x, 2.f) + pow(mousePos.y - projectedPos.y,2.f);
+        if (distance < 10.f)
+        {
+            hovering = true;
+        }
+        drawList.AddCircle(projectedPos, 1.f, ImGui::GetColorU32({1.f, 1.f, 1.f, 1.f}));
+
+        if (hovering) {
+            drawList.AddLine({screenPoint1.x, screenPoint1.y}, {screenPoint2.x, screenPoint2.y}, ImGui::GetColorU32({.25f, .25f, .25f, 1.f}));
+        }
+        else {
+            drawList.AddLine({screenPoint1.x, screenPoint1.y}, {screenPoint2.x, screenPoint2.y}, ImGui::GetColorU32({1.f, 1.f, 1.f, 1.f}));
         }
 
         return hovering;
--- ImGizmo.h
+++ ImGizmo.h
@@ -6,6 +6,100 @@
 #define IMGIZMO_NAMESPACE ImGizmo
 #endif
 
+struct ImGizmoMatrix;
+
+struct ImGizmoVec3 {
+    float x;
+    float y;
+    float z;
+
+    ImGizmoVec3() {
+        x = 0.f;
+        y = 0.f;
+        z = 0.f;
+    }
+
+    ImGizmoVec3(float _x, float _y, float _z) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    ImGizmoVec3 Transform(const ImGizmoMatrix& _matrix);
+};
+
+struct ImGizmoVec4 {
+    float x;
+    float y;
+    float z;
+    float w;
+
+    ImGizmoVec4() {
+        x = 0.f;
+        y = 0.f;
+        z = 0.f;
+        w = 0.f;
+    }
+
+    ImGizmoVec4(float _x, float _y, float _z, float _w) {
+        x = _x;
+        y = _y;
+        z = _z;
+        w = _w;
+    }
+
+    ImGizmoVec4 Transform(const ImGizmoMatrix& _matrix);
+};
+
+struct ImGizmoMatrix {
+    union {
+        float m4x4[4][4];
+        float m16[16];
+    };
+
+    ImGizmoMatrix() {
+        m4x4[0][0] = m4x4[0][1] = m4x4[0][2] = m4x4[0][3] =
+        m4x4[1][0] = m4x4[1][1] = m4x4[1][2] = m4x4[1][3] =
+        m4x4[2][0] = m4x4[2][1] = m4x4[2][2] = m4x4[2][3] =
+        m4x4[3][0] = m4x4[3][1] = m4x4[3][2] = m4x4[3][3] = 0.f;
+    };
+
+    ImGizmoMatrix(float* _matrix) {
+        m16[0] = _matrix[0];
+        m16[1] = _matrix[1];
+        m16[2] = _matrix[2];
+        m16[3] = _matrix[3];
+        m16[4] = _matrix[4];
+        m16[5] = _matrix[5];
+        m16[6] = _matrix[6];
+        m16[7] = _matrix[7];
+        m16[8] = _matrix[8];
+        m16[9] = _matrix[9];
+        m16[10] = _matrix[10];
+        m16[11] = _matrix[11];
+        m16[12] = _matrix[12];
+        m16[13] = _matrix[13];
+        m16[14] = _matrix[14];
+        m16[15] = _matrix[15];
+    }
+
+    static ImGizmoMatrix Indentity() {
+        ImGizmoMatrix m;
+
+        m.m4x4[0][1] = m.m4x4[0][2] = m.m4x4[0][3] =
+            m.m4x4[1][0] = m.m4x4[1][2] = m.m4x4[1][3] =
+            m.m4x4[2][0] = m.m4x4[2][1] = m.m4x4[2][3] =
+            m.m4x4[3][0] = m.m4x4[3][1] = m.m4x4[3][2] = 0.f;
+
+        m.m4x4[0][0] = m.m4x4[1][1] = m.m4x4[2][2] = m.m4x4[3][3] = 1.f;
+
+        return m;
+    };
+
+    ImGizmoVec3 Pos() const;
+    ImGizmoMatrix operator* (const ImGizmoMatrix& _matrix) const;
+};
+
 struct ImGizmoContext;
 
 namespace IMGIZMO_NAMESPACE {
@@ -19,8 +113,6 @@ namespace IMGIZMO_NAMESPACE {
 
     void AttachNextSpaceToViewport(ImGuiViewport* _viewport);
 
-    bool DrawTranslation(const char* _id, float* _matrix);
-    bool DrawScaling(const char* _id, float* _matrix);
-    bool DrawRotation(const char* _id, float* _matrix);
-    bool DrawPoint(const char* _id, float* _x, float* _y, float* _z);
+    bool DrawPoint(const char* _id, ImGizmoVec3 point);
+    bool DrawLine(const char* _id, ImGizmoVec3 point1, ImGizmoVec3 point2);
 }
