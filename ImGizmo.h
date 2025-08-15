60bd5dfcf2e1416d00ea9efc5dea0ad468eb4efc
Cornee Nieuwenhuis <37161255+Cinition@users.noreply.github.com>
2025-08-15 16:48:44 +0200

    Cleaner structure for header

--- ImGizmo.h
+++ ImGizmo.h
@@ -1,72 +1,27 @@
 #pragma once
 
-/*
-
-Index of this file:
-// [SECTION]
-
-*/
-
-// Configuration file with compile-time options
-// (edit imgizmoconfig.h or '#define IMGIZMO_USER_CONFIG "myfilename.h" from your build system)
-#ifdef IMGIZMO_USER_CONFIG
-#include IMGIZMO_USER_CONFIG
-#endif
-#include "imgizmoconfig.h"
-
 #include <imgui.h>
 
-struct ImVec3 {
-    float x, y, z;
-    constexpr ImVec3()                             : x(0.f), y(0.f), z(0.f) {}
-    constexpr ImVec3(float x, float y, float z) : x(x), y(y), z(z) {}
-    inline ImVec2 xy() const { return ImVec2(x, y); }
-#ifdef IM_VEC3_CLASS_EXTRA
-    IM_VEC3_CLASS_EXTRA     // Define additional constructors and implicit cast operators in imgizmoconfig.h to convert back and forth between your math types and ImVec3.
-#endif
-};
-
-struct ImMat44 {
-    union {
-        float m4x4[4][4];
-        float m16[16];
-    };
-
-    ImMat44() {
-        m4x4[0][0] = m4x4[0][1] = m4x4[0][2] = m4x4[0][3] =
-        m4x4[1][0] = m4x4[1][1] = m4x4[1][2] = m4x4[1][3] =
-        m4x4[2][0] = m4x4[2][1] = m4x4[2][2] = m4x4[2][3] =
-        m4x4[3][0] = m4x4[3][1] = m4x4[3][2] = m4x4[3][3] = 0.f;
-    };
-#ifdef IM_MAT44_CLASS_EXTRA
-    IM_MAT44_CLASS_EXTRA     // Define additional constructors and implicit cast operators in imgizmoconfig.h to convert back and forth between your matrices and ImMat44.
-#endif
-};
-
+struct ImVec3;
+struct ImMat44;
 struct ImGizmoContext;
 
 namespace ImGizmo {
 
-//-----------------------------------------------------------------------------
-// [SECTION] Context functions
-//-----------------------------------------------------------------------------
-
+    // Context functions
     ImGizmoContext* CreateContext();
     void DestroyContext(ImGizmoContext* context = nullptr);
     ImGizmoContext* GetCurrentContext();
     void SetCurrentContext(ImGizmoContext* context);
 
-//-----------------------------------------------------------------------------
-// [SECTION] Space
-//-----------------------------------------------------------------------------
-
-    bool Begin(const char* id, const ImMat44& view, const ImMat44& proj, const ImVec3& pos);
+    // Spaces
+    // Begin() = pushes a new Space/Window to context. Call End() to close the current Space/Window.
+    // - handles the same as an ImGui, Begin(). Meaning that Window Manipulation functions() work here aswell.
+    // - we calculate all manipulation and rendering in the End() call.
+    bool Begin(const char* id, const ImMat44& view, const ImMat44& proj, const ImVec3& cameraPosition);
     void End();
 
-//-----------------------------------------------------------------------------
-// [SECTION] Space utilities
-//-----------------------------------------------------------------------------
-
+    // Space utilities
     bool IsOver();
     bool IsUsing();
     const char* GetHoveredID();
@@ -75,64 +30,66 @@ namespace ImGizmo {
     ImVec2 GetUsingStartPos();
     ImVec2 GetLastMousePos();
 
-//-----------------------------------------------------------------------------
-// [SECTION] Space conversions
-//-----------------------------------------------------------------------------
+    // Space conversions
+    ImVec2 Convert3DTo2D(const ImVec3& pos);
+    ImVec3 Convert2DTo3D(const ImVec3& pos);
 
-    ImVec2 ConvertTo2DCoords(const ImVec3& pos);
+    // Drawing Primitives
+    bool DrawPoint(
+        const char* id,
+        const ImVec3& point,
+        float radius, float border,
+        ImU32 flags = 0, ImU32 color = 0xFFFFFFFF,
+        ImU32 borderColor = 0x000000FF
+    );
+    bool DrawLine(
+        const char* id,
+        const ImVec3& point1,
+        const ImVec3& point2,
+        float border,
+        ImU32 flags = 0,
+        ImU32 color = 0xFFFFFFFF,
+        ImU32 borderColor = 0x000000FF
+    );
+    bool DrawTriangle(
+        const char* id,
+        const ImVec3& point1,
+        const ImVec3& point2,
+        const ImVec3& point3,
+        float border,
+        ImU32 flags = 0,
+        ImU32 color = 0xFFFFFFFF,
+        ImU32 borderColor = 0x000000FF
+    );
+    bool DrawQuad(
+        const char* id,
+        const ImVec3& point1,
+        const ImVec3& point2,
+        const ImVec3& point3,
+        const ImVec3& point4,
+        float border,
+        ImU32 flags = 0,
+        ImU32 color = 0xFFFFFFFF,
+        ImU32 borderColor = 0x000000F
+    );
 
-//-----------------------------------------------------------------------------
-// [SECTION] Drawing Primitives
-//-----------------------------------------------------------------------------
-
-    bool DrawPoint(const char* id, const ImVec3& point, float radius, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF );
-    bool DrawPointWithOutline(const char* id, const ImVec3& point, float radius, float thickness, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF, ImU32 out_color = 0x000000FF);
-    bool DrawLine(const char* id, const ImVec3& point1, const ImVec3& point2, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF);
-    bool DrawLineWithOutline(const char* id, const ImVec3& point1, const ImVec3& point2, float thickness, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF, ImU32 out_color = 0x000000FF);
-    bool DrawTriangle(const char* id, const ImVec3& point1, const ImVec3& point2, const ImVec3& point3, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF);
-    bool DrawTriangleWithOutline(const char* id, const ImVec3& point1, const ImVec3& point2, const ImVec3& point3, float thickness, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF, ImU32 out_color = 0x000000FF);
-    bool DrawQuad(const char* id, const ImVec3& point1, const ImVec3& point2, const ImVec3& point3, const ImVec3& point4, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF);
-    bool DrawQuadWithOutline(const char* id, const ImVec3& point1, const ImVec3& point2, const ImVec3& point3, const ImVec3& point4, float thickness, ImU32 flags = 0, ImU32 color = 0xFFFFFFFF, ImU32 out_color = 0x000000FF);
-
-//-----------------------------------------------------------------------------
-// [SECTION] Space parameters stack
-//-----------------------------------------------------------------------------
-
-    void PushNextItemRotation(ImVec3* left, ImVec3* up, ImVec3* at);
-    void PushNextItemRotation(ImVec3* euler);
-    void PushNextItemRotation(ImVec4* quat);
-    void PopNextItemRotation();
-
-//-----------------------------------------------------------------------------
-// [SECTION] Gizmos
-//-----------------------------------------------------------------------------
-
-    bool Translate(const char* id, ImVec3* position);
-    bool Rotate(const char* id, ImVec3* position);
-    bool Scale(const char* id, ImVec3* position);
-
+    // Predefined Gizmos
+    bool Translate(
+        const char* id,
+        ImVec3* value,
+        ImVec3* position = nullptr,
+        ImVec3* rotation = nullptr
+    );
+    bool Rotate(
+        const char* id,
+        ImVec3* value,
+        ImVec3* position = nullptr,
+        ImVec3* rotation = nullptr
+    );
+    bool Scale(
+        const char* id,
+        ImVec3* value,
+        ImVec3* position = nullptr,
+        ImVec3* rotation = nullptr
+    );
 }
-
-// Helpers: ImVec3 operators
-// - These helper operators are implemented in the same way Dear ImGui implements their Vector operators.
-// - Add '#define IMGUI_DEFINE_MATH_OPERATORS' before including this file (or in imconfig.h) to access courtesy maths operators for ImVec2/ImVec3/ImVec4.
-#ifdef IMGUI_DEFINE_MATH_OPERATORS
-#define IMGUI_DEFINE_MATH_OPERATORS_IMPLEMENTED
-IM_MSVC_RUNTIME_CHECKS_OFF
-static inline ImVec3  operator*(const ImVec3& lhs, const float rhs)     { return ImVec3(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs); }
-static inline ImVec3  operator/(const ImVec3& lhs, const float rhs)     { return ImVec3(lhs.x / rhs, lhs.y / rhs, lhs.y / rhs); }
-static inline ImVec3  operator+(const ImVec3& lhs, const ImVec3& rhs)   { return ImVec3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z); }
-static inline ImVec3  operator-(const ImVec3& lhs, const ImVec3& rhs)   { return ImVec3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z); }
-static inline ImVec3  operator*(const ImVec3& lhs, const ImVec3& rhs)   { return ImVec3(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z); }
-static inline ImVec3  operator/(const ImVec3& lhs, const ImVec3& rhs)   { return ImVec3(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z); }
-static inline ImVec3  operator-(const ImVec3& lhs)                      { return ImVec3(-lhs.x, -lhs.y, -lhs.z); }
-static inline ImVec3& operator*=(ImVec3& lhs, const float rhs)          { lhs.x *= rhs; lhs.y *= rhs; lhs.z *= rhs; return lhs; }
-static inline ImVec3& operator/=(ImVec3& lhs, const float rhs)          { lhs.x /= rhs; lhs.y /= rhs; lhs.z /= rhs; return lhs; }
-static inline ImVec3& operator+=(ImVec3& lhs, const ImVec3& rhs)        { lhs.x += rhs.x; lhs.y += rhs.y; lhs.z += rhs.z;return lhs; }
-static inline ImVec3& operator-=(ImVec3& lhs, const ImVec3& rhs)        { lhs.x -= rhs.x; lhs.y -= rhs.y; lhs.z -= rhs.z;return lhs; }
-static inline ImVec3& operator*=(ImVec3& lhs, const ImVec3& rhs)        { lhs.x *= rhs.x; lhs.y *= rhs.y; lhs.z *= rhs.z;return lhs; }
-static inline ImVec3& operator/=(ImVec3& lhs, const ImVec3& rhs)        { lhs.x /= rhs.x; lhs.y /= rhs.y; lhs.z /= rhs.z;return lhs; }
-static inline bool    operator==(const ImVec3& lhs, const ImVec3& rhs)  { return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z; }
-static inline bool    operator!=(const ImVec3& lhs, const ImVec3& rhs)  { return lhs.x != rhs.x || lhs.y != rhs.y || lhs.z != rhs.z; }
-IM_MSVC_RUNTIME_CHECKS_RESTORE
-#endif
